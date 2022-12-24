cc1837abc70781d2dba4dd88d49e495cc54c7398
usama <mahmoodify@gmail.com>
2022-12-24 21:26:37 +0500

    input and output examples

---
+++ Chapter-03/01. Standard IO/Input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int val;
+
+    std::cout << "Enter value: ";
+    std::cin >> val;
+    std::cout << "You typed " << val << std::endl;
+
+    return 0;
+}
---
+++ Chapter-03/01. Standard IO/Output.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My Text ..." << std::endl;
+    return 0;
+}
