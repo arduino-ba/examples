cc9856357a2c1224d163b05da7aa72d4373c042b
usama <mahmoodify@gmail.com>
2022-12-25 22:04:29 +0500

    adding example

---
+++ Chapter-03/03-Switch/Switch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+    int option;
+
+    std::cout << "What do you want to see?" << std::endl;
+    std::cout << "  1 = Binaries" << std::endl;
+    std::cout << "  2 = Logging" << std::endl;
+    std::cout << "  3 = Data" << std::endl;
+    std::cout << "  4 = Support" << std::endl;
+    std::cout << "Give me a number: ";
+    std::cin >> option;
+
+    switch (option) {
+        case 1:
+            std::cout << "Displaying Binaries" << std::endl;
+            break;
+        case 2:
+            std::cout << "Displaying Logs" << std::endl;
+            break;
+        case 3:
+            std::cout << "Displaying Data" << std::endl;
+            break;
+        case 4:
+            std::cout << "Displaying Support Info" << std::endl;
+            break;
+        default:
+            std::cout << "Invalid option" << std::endl;
+            break;
+    }
+    
+    return 0;
+}
