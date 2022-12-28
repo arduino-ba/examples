174a4d66218f9b255b3ef4bd6df722f9844fd93e
usama <mahmoodify@gmail.com>
2022-12-28 23:26:36 +0500

    adding polymorphic example

---
+++ Chapter-04/06-Classes/04.Polymorphism.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Object {
+public:
+    virtual void draw() {
+        std::cout << "Object draw" << std::endl;
+    }
+};
+
+class Box : public Object {
+public:
+    void draw() override {
+        std::cout << "Box draw" << std::endl;
+    }
+};
+
+int main() {
+    Object *ptr1, *ptr2;
+    
+    ptr1 = new Object();
+    ptr2 = new Box();
+
+    ptr1->draw();
+    ptr2->draw();
+
+    return 0;
+}
