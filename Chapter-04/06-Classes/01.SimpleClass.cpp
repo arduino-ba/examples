bc8f9b8502364bcb0e47adb1a10bd1bbcf82a7d5
usama <mahmoodify@gmail.com>
2022-12-28 21:31:49 +0500

    adding simple class example

---
+++ Chapter-04/06-Classes/01.SimpleClass.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class ComplexNumber {
+private:
+    int real, complex;
+
+public:
+    void set(int r, int c) {
+        real = r;
+        complex = c;
+    }
+    void print() {
+        std::cout << real << " + "
+                  << complex << "j" << std::endl;
+    }
+};
+
+int main() {
+    ComplexNumber cn1, cn2;
+    cn1.set(1, 2);
+    cn2.set(5, 8);
+
+    cn1.print();
+    cn2.print();
+
+    return 0;
+}
