e03b02176c6aaf6d5432fe7a7f079a4dff69da09
usama <mahmoodify@gmail.com>
2022-12-28 16:52:01 +0500

    adding pointer srith example

---
+++ Chapter-04/05-Pointers/PointerArithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int values[] = {1, 2, 3, 4, 5};
+    
+    int *int_ptr = &values[0];
+
+    std::cout << "Output format = Address:Value "
+              << std::endl << std::endl;
+    
+    std::cout << int_ptr << ":" << *int_ptr << std::endl;
+    
+    int_ptr++;
+    std::cout << int_ptr << ":" << *int_ptr << std::endl;
+
+    int_ptr += 1;
+    std::cout << int_ptr << ":" << *int_ptr << std::endl;
+
+    std::cout << int_ptr + 1 << ":" << *(int_ptr + 1) << std::endl;
+    
+    return 0;
+}
