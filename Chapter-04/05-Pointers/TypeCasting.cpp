b57a4b963ae1f52f0b17a73066bfede829f37f03
usama <mahmoodify@gmail.com>
2022-12-28 17:19:06 +0500

    adding typecasting example

---
+++ Chapter-04/05-Pointers/TypeCasting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    int int_value = 0x00FFEEDD;
+    double double_value = 3.14159;
+    unsigned char *ptr;
+
+    // Integer's bytes:
+    ptr = (unsigned char *)&int_value;
+
+    std::cout << int_value << " is placed in memory as: ";
+
+    for (int i = 0; i < sizeof(int_value); i++) {
+        std::cout << std::hex << (int)*ptr++ << " ";
+    }
+
+    std::cout << std::endl;
+
+    // Double's bytes:
+    ptr = (unsigned char *)&double_value;
+
+    std::cout << double_value << " is placed in memory as:  ";
+
+    for (int i = 0; i < sizeof(double_value); i++) {
+        std::cout << std::hex << (int)*ptr++ << " ";
+    }
+
+    std::cout << std::endl;
+    
+    return 0;
+}
