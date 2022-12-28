58846d76b6dde0ad7ad5a897517ebd03aede1fb9
usama <mahmoodify@gmail.com>
2022-12-28 21:57:01 +0500

    adding ctor example

---
+++ Chapter-04/06-Classes/02.ConstructorDestr.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class ComplexNumber {
+private:
+    int real, complex;
+
+public:
+    ComplexNumber(int r, int c) {
+        real = r;
+        complex = c;
+    }
+    void print() {
+        std::cout << real << " + "
+                  << complex << "j" << std::endl;
+    }
+};
+
+int main() {
+    ComplexNumber cn1(1, 2), cn2(5, 8);
+
+    cn1.print();
+    cn2.print();
+
+    return 0;
+}
