b0a40cc4da04cce8691891975e07ae8108a752e1
usama <mahmoodify@gmail.com>
2022-12-29 18:50:23 +0500

    adding example

---
+++ Chapter-04/10-MultipleCodeFiles/my_library.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int get_number_from_user() {
+    int value;
+    cin >> value;
+    return value;
+}
+
+void print_table(int t) {
+    for (int i = 1; i <= 10; i++) {
+        cout << t << " x "
+             << i << " = "
+             << t * i << endl;
+    }
+}
---
+++ Chapter-04/10-MultipleCodeFiles/my_library.h
@@ -0,0 +1,7 @@
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
+
+int get_number_from_user();
+void print_table(int);
+
+#endif
