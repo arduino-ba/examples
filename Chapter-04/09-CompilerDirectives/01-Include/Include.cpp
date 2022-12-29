01211c016c6c0dce0da3441862a403726f7674c7
usama <mahmoodify@gmail.com>
2022-12-29 16:16:01 +0500

    adding example

---
+++ Chapter-04/09-CompilerDirectives/01-Include/Include.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int my_array[] = {
+        #include "values.csv"
+    };
+
+    for (int i = 0;
+         i < sizeof(my_array)/sizeof(int);
+         i++) {
+        
+        cout << my_array[i] << " - ";
+    }
+
+    cout << endl;
+    
+    return 0;
+}
