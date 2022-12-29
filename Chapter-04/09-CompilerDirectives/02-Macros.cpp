1eab2e7cd4172ff2068fb30117823618aa774b04
usama <mahmoodify@gmail.com>
2022-12-29 17:24:12 +0500

    updating

--- Chapter-04/09-CompilerDirectives/02-Macros.cpp
+++ Chapter-04/09-CompilerDirectives/02-Macros.cpp
@@ -9,7 +9,7 @@ int function_div(int v1, int v2) { return v1 / v2; }
 
 #define V1   20
 #define V2   10
-#define op(number,value1,value2)  function_##number(value1,value2)
+#define op(oper,value1,value2)  function_##oper(value1,value2)
 
 int main() {
     cout << V1 << " + " << V2 << " = " << op(add, V1, V2) << endl;
