4618dff9805efe670f4d2116cfd9db1c5b35a1b4
usama <mahmoodify@gmail.com>
2022-12-29 17:13:58 +0500

    adding macros example

---
+++ Chapter-04/09-CompilerDirectives/02-Macros.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int function_add(int v1, int v2) { return v1 + v2; }
+int function_sub(int v1, int v2) { return v1 - v2; }
+int function_mul(int v1, int v2) { return v1 * v2; }
+int function_div(int v1, int v2) { return v1 / v2; }
+
+#define V1   20
+#define V2   10
+#define op(number,value1,value2)  function_##number(value1,value2)
+
+int main() {
+    std::cout << V1 << " + " << V2 << " = " << op(add, V1, V2) << std::endl;
+    std::cout << V1 << " - " << V2 << " = " << op(sub, V1, V2) << std::endl;
+    std::cout << V1 << " x " << V2 << " = " << op(mul, V1, V2) << std::endl;
+    std::cout << V1 << " / " << V2 << " = " << op(div, V1, V2) << std::endl;
+
+    return 0;
+}
