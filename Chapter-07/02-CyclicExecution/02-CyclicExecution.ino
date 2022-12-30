54325d1bafb5221bb666a0daeb9c66ebc73f3510
usama <mahmoodify@gmail.com>
2022-12-30 15:30:19 +0500

    adding cyclic task execution

---
+++ Chapter-07/02-CyclicExecution/02-CyclicExecution.ino
@@ -0,0 +1,28 @@
+#include "CyclicTaskExecutor.h"
+
+using namespace cblk;
+
+CyclicTaskExecutor* exe_1;
+CyclicTaskExecutor* exe_2;
+
+void task_1() {
+  Serial.print(".");
+}
+
+void task_2() {
+  Serial.print("|");
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  exe_1 = new CyclicTaskExecutor(task_1, 1000);
+  exe_2 = new CyclicTaskExecutor(task_2, 5000);
+}
+
+void loop() {
+  unsigned long currentTime_ms = millis();
+  
+  exe_1->update(currentTime_ms);
+  exe_2->update(currentTime_ms);
+}
---
+++ Chapter-07/02-CyclicExecution/CyclicTaskExecutor.h
@@ -0,0 +1,17 @@
+namespace cblk {
+  typedef void(*func_ptr_t)();
+  
+  class CyclicTaskExecutor {
+    private:
+    
+    func_ptr_t funcPtr;
+    unsigned int callingInterval_ms;
+    unsigned long lastCalledTime_ms;
+    
+    public:
+    
+    CyclicTaskExecutor(func_ptr_t func_ptr, unsigned int calling_interval_ms, bool call_on_zero = false);
+    void change_interval(unsigned int calling_interval_ms, bool call_on_zero = false);
+    void update(unsigned long current_time_ms);
+  };
+}
