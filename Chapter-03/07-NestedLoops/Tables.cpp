e9ca6eba46cecaa0fcbf455a193fd4b185f11533
μςαⓂα <mahmoodify@gmail.com>
2023-01-06 14:26:59 +0500

    Update Tables.cpp

--- Chapter-03/07-NestedLoops/Tables.cpp
+++ Chapter-03/07-NestedLoops/Tables.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 
+
 int main() {
     int table;
 
+    
     while (1) {
+        
         std::cout << "Table of what? (0 or negative to exit) ";
         std::cin >> table;
 
+        
         if (table <= 0) break;
 
+        
         for (int n = 1; n <= 10; n++) {
+            
             std::cout << table << " x " << n << " = "
                       << table * n << std::endl;
+            
         }
     }
+    
     
     return 0;
 }
