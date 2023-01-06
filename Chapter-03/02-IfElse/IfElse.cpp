0eb8002a865cd64d35cc213f06adf5eb1e3a59b5
μςαⓂα <mahmoodify@gmail.com>
2023-01-06 14:22:08 +0500

    Update IfElse.cpp

--- Chapter-03/02-IfElse/IfElse.cpp
+++ Chapter-03/02-IfElse/IfElse.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 
+
 int main() {
     int age;
 
     std::cout << "What is your age? ";
     std::cin >> age;
 
+    
     if (age < 0) {
+        
         std::cout << "Age cannot be negative." << std::endl;
+        
     } else if (age == 0) {
+        
         std::cout << "Are you just born?" << std::endl;
+        
     } else if (age <= 10) {
+        
         std::cout << "Let us play" << std::endl;
+        
     } else if (age <= 20) {
+        
         std::cout << "Let us study" << std::endl;
+        
     } else {
+        
         std::cout << "Enjoy!" << std::endl;
+        
     }
+    
     
     return 0;
 }
