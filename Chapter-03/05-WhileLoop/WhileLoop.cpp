01253385201d598d36fb536257925a7c3d2b9bcb
μςαⓂα <mahmoodify@gmail.com>
2023-01-06 14:25:15 +0500

    Update WhileLoop.cpp

--- Chapter-03/05-WhileLoop/WhileLoop.cpp
+++ Chapter-03/05-WhileLoop/WhileLoop.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 
+
 int main() {
     int table;
 
     std::cout << "Table of what? ";
     std::cin >> table;
 
+    
     int n = 1;
+    
     while (n <= 10) {
+        
         std::cout << table << " x " << n << " = "
                   << table * n << std::endl;
+        
         n++;
+        
     }
 
+    
     return 0;
 }
