baef8a5d3c6ac6b7689c5c11111d92399f1a1410
μςαⓂα <mahmoodify@gmail.com>
2023-01-06 14:24:21 +0500

    Update ForLoop.cpp

--- Chapter-03/04-ForLoop/ForLoop.cpp
+++ Chapter-03/04-ForLoop/ForLoop.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
+
 int main() {
     int table;
 
     std::cout << "Table of what? ";
     std::cin >> table;
 
+    
     for (int n = 1; n <= 10; n++) {
+        
         std::cout << table << " x " << n << " = "
                   << table * n << std::endl;
+        
     }
 
+    
     return 0;
 }
