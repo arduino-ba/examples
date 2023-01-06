f02504f88bc27e8c2ce3fe6ce68a05ae9b8df84b
μςαⓂα <mahmoodify@gmail.com>
2023-01-06 14:26:12 +0500

    Update DoWhileLoop.cpp

--- Chapter-03/06-DoWhileLoop/DoWhileLoop.cpp
+++ Chapter-03/06-DoWhileLoop/DoWhileLoop.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
+
 int main() {
     int value;
 
     std::cout << "Enter 5 to exit: " << std::endl;
 
+    
     do {
+        
         std::cout << "  Value?: ";
         std::cin >> value;
+        
     } while (value != 5);
+    
     
     return 0;
 }
