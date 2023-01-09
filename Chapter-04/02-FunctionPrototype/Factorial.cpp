5468954e797a1b5543582fdbe487e9fc942e2e3c
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:19:47 +0500

    Update Factorial.cpp

--- Chapter-04/02-FunctionPrototype/Factorial.cpp
+++ Chapter-04/02-FunctionPrototype/Factorial.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
+
 int factorial(int number);
+
 
 int main() {
     int value;
+    
     while (1) {
+        
         std::cout << "Factorial of? (0 or negative to exit) ";
         std::cin >> value;
 
+        
         if (value <= 0) break;
 
+        
         std::cout << "  " << value << "! = "
                   << factorial(value) << std::endl;
+        
     }
     
     return 0;
 }
+
 
 int factorial(int number) {
     if (number <= 1) return 1;
