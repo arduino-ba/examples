60c926f7e42e32759d96d8e2b485546603425023
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:45:27 +0500

    Update KeypadReader.h

--- Chapter-07/01-KeypadInput/KeypadReader.h
+++ Chapter-07/01-KeypadInput/KeypadReader.h
@@ -1,8 +1,11 @@
 namespace cblk {
+  
   typedef void(*keypad_callback_t)(char);
+  
   
   class KeypadReader {
     private:
+    
     keypad_callback_t onKeyDown = nullptr;
     keypad_callback_t onKeyUp   = nullptr;
 
@@ -10,16 +13,24 @@ namespace cblk {
 
     unsigned char rows;
     unsigned char cols;
+    
     const unsigned char* rowPins;
     const unsigned char* colPins;
+    
     const char* keyMap;
+    
     char* keysStatus;
     
+    
+    
     public:
+    
     KeypadReader(keypad_callback_t on_key_down, keypad_callback_t on_key_up,
                  const unsigned char num_rows, const unsigned char num_cols,
                  const unsigned char* row_pins, const unsigned char* col_pins,
                  const char* key_map);
+    
     void update();
   };
+  
 }
