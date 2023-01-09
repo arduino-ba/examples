c135a7495736c7a5143b05351587ece9a5735aba
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:32:39 +0500

    Update 02.Namespaces.cpp

--- Chapter-04/07-Namespaces/02.Namespaces.cpp
+++ Chapter-04/07-Namespaces/02.Namespaces.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 
+
 using namespace std;
 
+
 namespace ns1 {
+    
     namespace ns2 {
+        
         void print_hello_1() {
             cout << "Hello World 1" << endl;
         }
+        
     }
 
+    
     void print_hello_2() {
         cout << "Hello World 2" << endl;
     }
+    
 }
+
 
 int main() {
     ns1::ns2::print_hello_1();
