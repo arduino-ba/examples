5fde5d8dc8a02ec7c1e9ec283e19e9d9d088d3dd
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:34:24 +0500

    Update 02.Using.cpp

--- Chapter-04/08-TypeAliases/02.Using.cpp
+++ Chapter-04/08-TypeAliases/02.Using.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 
+
 using Number = int;
 using FunctionPtr = Number(*)(Number, Number);
+
 
 Number add(Number a, Number b) {
     return a + b;
 }
+
 Number subtract(Number a, Number b) {
     return a - b;
 }
+
 Number multiply(Number a, Number b) {
     return a * b;
 }
+
 Number divide(Number a, Number b) {
     return a / b;
 }
 
+
 Number op(Number a, Number b, FunctionPtr func) {
     return func(a, b);
 }
+
 
 int main() {
     std::cout << "20 + 10 = " << op(20, 10, add) << std::endl;
