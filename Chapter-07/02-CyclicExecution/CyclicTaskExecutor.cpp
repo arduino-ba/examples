65a2578f0e6b1db76b3b9c911ec31ac9ff65d288
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:58:41 +0500

    Update CyclicTaskExecutor.cpp

--- Chapter-07/02-CyclicExecution/CyclicTaskExecutor.cpp
+++ Chapter-07/02-CyclicExecution/CyclicTaskExecutor.cpp
@@ -1,34 +1,51 @@
 #include <Arduino.h>
 #include "CyclicTaskExecutor.h"
 
+
 #define MAX_MILLIS (unsigned long)0xffffffff
+
 
 cblk::CyclicTaskExecutor::CyclicTaskExecutor(func_ptr_t func_ptr, unsigned int calling_interval_ms, bool call_on_zero) {
   funcPtr = func_ptr;
   change_interval(calling_interval_ms, call_on_zero);
 }
 
+
 void cblk::CyclicTaskExecutor::change_interval(unsigned int calling_interval_ms, bool call_on_zero = false) {
   callingInterval_ms = calling_interval_ms;
 
+  
   if (call_on_zero) {
+    
     if (funcPtr != NULL) funcPtr();
+    
   }
+  
   
   lastCalledTime_ms = millis();
 }
+
 
 void cblk::CyclicTaskExecutor::update(unsigned long current_time_ms) {
   unsigned long elapsedTime_ms = 0;
   
   if (current_time_ms < lastCalledTime_ms) {
+    
     elapsedTime_ms = (MAX_MILLIS - lastCalledTime_ms) + current_time_ms;
+    
   } else {
+    
     elapsedTime_ms = current_time_ms - lastCalledTime_ms;
+    
   }
 
+  
   if (elapsedTime_ms >= callingInterval_ms) {
+    
     lastCalledTime_ms = current_time_ms;
+    
     if (funcPtr != NULL) funcPtr();
+    
   }
+  
 }
