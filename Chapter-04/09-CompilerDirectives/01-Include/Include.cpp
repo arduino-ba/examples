32d8e6f37a5064b57c58b431b3fcde73731affd4
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:36:21 +0500

    Update Include.cpp

--- Chapter-04/09-CompilerDirectives/01-Include/Include.cpp
+++ Chapter-04/09-CompilerDirectives/01-Include/Include.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 
+
 using namespace std;
 
+
 int main() {
+    
     int my_array[] = {
+        
         #include "values.csv"
+        
     };
 
+    
     for (int i = 0;
          i < sizeof(my_array)/sizeof(int);
          i++) {
         
         cout << my_array[i] << " - ";
+        
     }
 
+    
     cout << endl;
     
     return 0;
