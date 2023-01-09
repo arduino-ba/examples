a4a4c79d68ad0e9719566e3ddedce69d37e90aeb
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:56:10 +0500

    Update 02-CyclicExecution.ino

--- Chapter-07/02-CyclicExecution/02-CyclicExecution.ino
+++ Chapter-07/02-CyclicExecution/02-CyclicExecution.ino
@@ -1,6 +1,8 @@
 #include "CyclicTaskExecutor.h"
 
+
 using namespace cblk;
+
 
 CyclicTaskExecutor* exe_1;
 CyclicTaskExecutor* exe_2;
@@ -13,12 +15,14 @@ void task_2() {
   Serial.print("|");
 }
 
+
 void setup() {
   Serial.begin(115200);
   
   exe_1 = new CyclicTaskExecutor(task_1, 1000);
   exe_2 = new CyclicTaskExecutor(task_2, 5000);
 }
+
 
 void loop() {
   unsigned long currentTime_ms = millis();
