ef5b23f8cbea77e91ac7781d704f92b0130eb97e
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:31:21 +0500

    Update 03.Inheritance.cpp

--- Chapter-04/06-Classes/03.Inheritance.cpp
+++ Chapter-04/06-Classes/03.Inheritance.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 class Object {
 private:
     int x, y;
@@ -7,18 +8,23 @@ private:
 protected:
     Object(int x_value, int y_value) : x(x_value), y(y_value) {
     }
+    
     int get_x() { return x; }
+    
     int get_y() { return y; }
 };
+
 
 class Box : public Object {
 private:
     int w, h;
+    
 public:
     Box(int x, int y, int width, int height):Object(x, y){
         w = width;
         h = height;
     }
+    
     void draw() {
         std::cout << "Box: at "
                   << get_x() << "," << get_y()
@@ -27,8 +33,10 @@ public:
     }
 };
 
+
 int main() {
     Box b1(1, 2, 5, 5), b2(5, 9, 8, 8);
+    
     b1.draw();
     b2.draw();
 
