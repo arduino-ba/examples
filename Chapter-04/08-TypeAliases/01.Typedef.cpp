b20eaac4d8907cc4e9460e1f9137a1ad330cbfbf
μςαⓂα <mahmoodify@gmail.com>
2023-01-09 19:33:30 +0500

    Update 01.Typedef.cpp

--- Chapter-04/08-TypeAliases/01.Typedef.cpp
+++ Chapter-04/08-TypeAliases/01.Typedef.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 
+
 typedef int Integer;
 typedef Integer(*FunctionPtr)(Integer, Integer);
+
 
 Integer add(Integer a, Integer b) {
     return a + b;
 }
+
 Integer subtract(Integer a, Integer b) {
     return a - b;
 }
+
 Integer multiply(Integer a, Integer b) {
     return a * b;
 }
+
 Integer divide(Integer a, Integer b) {
     return a / b;
 }
 
+
 Integer op(Integer a, Integer b, FunctionPtr func) {
     return func(a, b);
 }
+
 
 int main() {
     std::cout << "20 + 10 = " << op(20, 10, add) << std::endl;
